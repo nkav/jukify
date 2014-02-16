7b0ebef075d03b36bbbeb8b99e2a6cd8ad71c1ea
HakoSwede <rollenseb@gmail.com>
2014-02-15 23:36:57 -0500

    Moved token to function

--- pennapps/fb_api.py
+++ pennapps/fb_api.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 from pyfb import Pyfb
-from settings import FACEBOOK_APP_ID, FACEBOOK_SECRET_KEY, RDIO_KEY, RDIO_SECRET
+from settings import FACEBOOK_APP_ID, FACEBOOK_SECRET_KEY, RDIO_KEY, RDIO_SECRET, LAST_API_KEY, LAST_SECRET, LAST_USERNAME, LAST_PASSWORD
 import sys
 from collections import Counter
 import json
 from rdio import Rdio
+import pylast
+
 
 fb = Pyfb(FACEBOOK_APP_ID)
 rdio = Rdio((RDIO_KEY, RDIO_SECRET))
+NUMBER_OF_PEOPLE = 50
 
+API_KEY = LAST_API_KEY 
+API_SECRET = LAST_SECRET
+
+username = LAST_USERNAME
+password_hash = pylast.md5(LAST_PASSWORD)
+
+network = pylast.LastFMNetwork(api_key = API_KEY, api_secret = 
+    API_SECRET, username = username, password_hash = password_hash)
+	
 #Copy the [access_token] and enter it below
 
-token = 'CAAUz0srmPAUBAAVpXbhH36G3CZBHlWqY95va4nRemDL7cglgdVkhKDOLoptlZBe5KuXKZB37xMtztCxcPttK2qhXzVoYYSrHPTn3Uykr10axK11SZCrlbdnH2HbswtpZAQLrbr6qB0YmdNrJ0DX8cAaEE7EbEPt6fub8KfttXplsA0cMkjode0GoGaQ6PKDjYp0cBq1o5rgZDZD'
+token = 'CAAUz0srmPAUBAM24zlcMKhZCOYGokKejgogMHoGTduZCayr00HFGUvpiOEdP0J9JmWFlprkSZCPHD7H6FFVF7TRf7FLcGEs6kZBWb1WTZASLwe4kX8RdF9qIug1VhF0jNZAE8HXgakJBaoER3bKdG9wIRAMAVye5BpEy3BwLjsvzWFSauYyS6v94jsCYS6MS3epNLZCEB74AAZDZD'
 #Sets the authentication token
-fb.set_access_token(token)
+
 	
 def find_artist_id(artist_name):
 	x=rdio.call('search',params={'query': artist_name, 'types': 'Artist'})
@@ -42,15 +54,22 @@ def find_songs(artist):
 		z = y[i]
 		songlist.append(z['name'].encode(sys.stdout.encoding, errors='replace'))
 	return songlist
+
+def get_genre(artist):
+	artist = network.get_artist(artist)
+	topItems = artist.get_top_tags(limit=None)
+	for topItem in topItems:
+		return topItem.item.get_name().encode(sys.stdout.encoding, errors='replace')
 	
-def get_artists(user_id):
+def get_artists(user_id, token):
+	fb.set_access_token(token)
 	dictionary = {}
 	list = []
 	dictionarylist = []
 	i = 0
 	friends = fb.get_friends(user_id)
 	for friend in friends:
-		if i < 50:
+		if i < NUMBER_OF_PEOPLE:
 			a = fb.get_likes(friend.id)
 			while (a):
 				for like in a:
@@ -62,7 +81,8 @@ def get_artists(user_id):
 	b = Counter(list)
 	i=0
 	for artist in b:
-		if b[artist] > 4:
+		if b[artist] > (NUMBER_OF_PEOPLE/20):
+			print artist, i
 			dictionary["likes"] = b[artist]
 			dictionary["name"] = artist
 			try:
@@ -77,9 +97,12 @@ def get_artists(user_id):
 				dictionary["imageUrl"] = find_artist_image(artist)
 			except:
 				dictionary["imageUrl"] = ""
+			try:
+				dictionary["genre"] = get_genre(artist)
+			except:
+				dictionary["genre"] = ""
 			dictionarylist.append(dictionary.copy())
 			i=i+1
-			print i
 	return dictionarylist
 	
 def format_text(list):
@@ -91,7 +114,7 @@ def format_text(list):
 		for key in item:
 			f.write("\t\t%r: %r" % (key.encode(sys.stdout.encoding, errors='replace'), item[key]))
 			counter=counter+1
-			if counter == 5:
+			if counter == 6:
 				f.write("\n")
 				break
 			f.write(",\n")
@@ -110,4 +133,5 @@ def json_list(list):
 	return json.dumps(list, sort_keys = True, indent = 4)
 	
 	
-#print json_list(get_artists('sebastian.rollen'))
+f=open("sample.txt","w")
+f.write(json_list(get_artists('sebastian.rollen', token)))
